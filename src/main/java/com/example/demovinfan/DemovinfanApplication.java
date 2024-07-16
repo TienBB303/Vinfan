91fc08821b9e70d3573404ba081d2911046abbdd
TienBB303 <tienbbph31709@fpt.edu.vn>
2024-07-16 14:50:59 +0700

    first commit

---
+++ src/main/java/com/example/demovinfan/DemovinfanApplication.java
@@ -0,0 +1,13 @@
+package com.example.demovinfan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemovinfanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemovinfanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demovinfan/DemovinfanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demovinfan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemovinfanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
